b6b48c99b5ea75b1fad34b7aca5bce0ca86e9634
Tautvydas-coder <b7&*%13kHqvP>
2022-08-26 10:11:35 +0300

    added TODO

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# TODO
+# TODO open URL
+# TODO input in the field three different cities
+# TODO grab nearest 3 days temperature
+# TODO all info should be in the Dict
+# TODO save all info in the JSON file
+
