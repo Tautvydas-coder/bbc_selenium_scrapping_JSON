4d1627d66047342639bfa96a0b79f47c5d78988d
Tautvydas-coder <b7&*%13kHqvP>
2022-08-26 10:14:18 +0300

    added TODO

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 # TODO input in the field three different cities
 # TODO grab nearest 3 days temperature
 # TODO all info should be in the Dict
-# TODO save all info in the JSON file
+# TODO save all info in the JSON file (e.g. info->goal_weather.txt)
 
