abc4b9022c687dfdab4079a4330b134f126235ec
Tautvydas-coder <b7&*%13kHqvP>
2022-08-31 14:21:54 +0300

    working: fetch temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium_resources.parser import *
 from json_resources.json_format import *
-# TODO grab nearest 3 days temperature
+
 # TODO all info should be in the Dict
 # TODO save all info in the JSON file (e.g. info->weather_chal.txt)
 
