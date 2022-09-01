aa0090156481f912fc275076ed10f86fe10ff493
Tautvydas-coder <b7&*%13kHqvP>
2022-09-01 12:49:13 +0300

    deleted temporary variables

--- selenium_resources/parser.py
+++ selenium_resources/parser.py
@@ -36,12 +36,10 @@ def search_bar():
         ActionChains(driver).move_to_element(input_bar).click().perform()
         ActionChains(driver).send_keys(input_city(value)).perform()
         ActionChains(driver).move_to_element(search_button).double_click().perform()
-        value += 1
         time.sleep(1)
-
-        wather_info = fetch_weather_information()
-        days_weather_full_info.append(wather_info)
-
+        weather_info = fetch_weather_information()
+        days_weather_full_info.append(weather_info)
+        value += 1
     return days_weather_full_info
 
 
@@ -54,35 +52,26 @@ def input_city(value):
 def fetch_weather_information():
     index = 1
     day_weather_info = []
-    days_weather_full_info = []
     while index < 4:
         WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH, f'//*[@id="daylink-{index}"]'))).click()
-
         day_element = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH, f'//*[@id="daylink-{index}"]/div[2]')))
         day = list(day_element.get_attribute('aria-label').split(" "))[0]
-        print(day)
-
         temperature_element = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.CSS_SELECTOR,
                                             f'#daylink-{index} > div.wr-day__body > div.wr-day__details-container > div > div.wr-day__temperature > div > div.wr-day-temperature__high > span.wr-day-temperature__high-value > span > span.wr-value--temperature--c')))
         temperature = temperature_element.text
-        print(temperature)
-
         description_element = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH,
                                             f'//*[@id="daylink-{index}"]/div[4]/div[2]/div')))
         description = description_element.text
-        print(description)
-        index += 1
-
         day_weather_info.append(day)
-        day_weather_info.append(temperature)
+        day_weather_info.append(temperature.split("°")[0])
         day_weather_info.append(description)
-        print(day_weather_info)
-
+        index += 1
     return day_weather_info
+
 
 def close_windows():
     driver.close()
